ad89040f693a4eb76e4e0cb4f220967882c2545b
Chiffa <artemy040404@gmail.com>
2024-11-29 22:48:59 +0300

    the very basics

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
-use futures_util::{future, StreamExt, TryStreamExt};
+use futures_util::{future, SinkExt, StreamExt, TryStreamExt};
 use rosu_v2::prelude::*;
 use serde::Deserialize;
+use std::sync::Arc;
 use std::{borrow::Borrow, fs::File, io::Read, thread, time::Duration};
 use tokio::net::{TcpListener, TcpStream};
 use tokio_tungstenite::accept_async;
-use std::sync::Arc;
+use tokio_tungstenite::tungstenite::Message;
 
 #[derive(Deserialize)]
 struct Api {
@@ -21,19 +22,18 @@ async fn main() {
         .await
         .unwrap();
 
-    let osu_ptr = Arc::new(osu)
+    let osu_ptr = Arc::new(osu);
 
-    let addr = "127.0.0.1:8080".to_string();
+    let addr = "127.0.0.1:7272".to_string();
     let try_socket = TcpListener::bind(&addr).await;
     let listener = try_socket.expect("Failed to bind");
 
     while let Ok((stream, _)) = listener.accept().await {
+        let ptr = osu_ptr.clone();
         tokio::spawn(async {
-            accept_connection(stream, osu_ptr.clone());
-            thread::sleep(Duration::from_secs(1));
+            accept_connection(stream, ptr).await;
         });
     }
-
 }
 
 async fn accept_connection(stream: TcpStream, osu: Arc<Osu>) {
@@ -42,11 +42,23 @@ async fn accept_connection(stream: TcpStream, osu: Arc<Osu>) {
         .expect("Connected streams should have a peer address");
     let ws_stream = accept_async(stream).await.expect("Error during handshake");
 
-    let (write, read) = ws_stream.split();
+    let (mut write, read) = ws_stream.split();
     let scores: Vec<Score> = osu
         .user_scores("Chiffa")
         .mode(GameMode::Osu)
         .best()
         .await
         .unwrap();
+    loop {
+        let w = write
+            .send(Message::Text(serde_json::to_string(&scores).unwrap()))
+            .await;
+        match w {
+            Ok(wr) => thread::sleep(Duration::from_secs(1)),
+            Err(e) => {
+                eprintln!("{e}");
+                break;
+            }
+        }
+    }
 }
